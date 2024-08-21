28967fd17feee6d4c4034321b2e1b4763f575a31
Restyled.io <commits@restyled.io>
2024-08-21 11:07:13 +0000

    Restyled by autopep8

--- pages/Zone_de_Gestion.py
+++ pages/Zone_de_Gestion.py
@@ -5,7 +5,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from streamlit_geolocation import streamlit_geolocation
 
-from app.db_managers import add_management_zone , get_management_zones , get_events_by_zone , add_event, delete_all_zone_events_by_type
+from app.db_managers import add_management_zone, get_management_zones, get_events_by_zone, add_event, delete_all_zone_events_by_type
 from app.watering import generate_detailed_watering_plan
 from app.weather import get_weather
 from streamlit_calendar import calendar
@@ -32,20 +32,23 @@ custom_css = """
             }
         """
 st.set_page_config(
-        page_title="Mazao",
-        page_icon=None,
-        layout="wide",
-        initial_sidebar_state="auto",
-        menu_items=None,
+    page_title="Mazao",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None,
 )
+
 
 @st.cache_data
 def get_zone_data(zone_id: int, zone_name: str, crop_type: str, longitude: float, latitude: float):
     return zone_id, zone_name, crop_type, longitude, latitude
 
+
 @st.cache_data
 def get_weather_data(city: str):
     return get_weather(city)
+
 
 # Initialize session state
 if 'current_zone' not in st.session_state:
@@ -54,11 +57,12 @@ if 'current_zone' not in st.session_state:
 if 'watering_plan' not in st.session_state:
     st.session_state.watering_plan = None
 
+
 @st.dialog("Créer une nouvelle zone de gestion", width="large")
 def new_zone():
     name = st.text_input("Nom")
     crop_type = st.selectbox(
-            "Type de culture", ["Maïs", "Tomate", "Piment", "Pomme de terre"]
+        "Type de culture", ["Maïs", "Tomate", "Piment", "Pomme de terre"]
     )
     st.write("Localisation")
     loc1, loc2 = st.columns([8, 2])
@@ -76,6 +80,7 @@ def new_zone():
         add_management_zone(name, crop_type, longitude, latitude)
         st.rerun()
 
+
 @st.dialog("Ajouter un événement", width="large")
 def add_new_event():
     name = st.text_input("Nom")
@@ -90,8 +95,10 @@ def add_new_event():
         event_type = "blue"
     description = st.text_area("Description")
     if st.button("Ajouter"):
-        add_event(name, datetime.datetime.combine(date, time), st.session_state.current_zone.id, event_type, description)
+        add_event(name, datetime.datetime.combine(date, time),
+                  st.session_state.current_zone.id, event_type, description)
         st.rerun()
+
 
 @st.dialog("Analyser une image", width="large")
 def analyze_image():
@@ -113,9 +120,11 @@ def analyze_image():
             st.write("*Suggestion de traitement:*")
             for traitement in disease_data["traitement"]:
                 st.write(f"- {traitement}")
-            st.write(f"*Suggestion traitement chimique:* {disease_data['suggestion_traitement_chimique']}")
+            st.write(
+                f"*Suggestion traitement chimique:* {disease_data['suggestion_traitement_chimique']}")
         else:
             st.success("La plante est saine!")
+
 
 st.title("Weather App")
 
@@ -141,18 +150,18 @@ with col1:
 with col2:
     if st.session_state.current_zone:
         zone = st.session_state.current_zone
-        zone_id, zone_name, crop_type, longitude, latitude = get_zone_data(zone.id, zone.name, zone.crop_type, zone.longitude, zone.latitude)
-
-
+        zone_id, zone_name, crop_type, longitude, latitude = get_zone_data(
+            zone.id, zone.name, zone.crop_type, zone.longitude, zone.latitude)
 
         st.write(f"# {zone_name}")
 
-        temp, humidity, _ = get_weather_data(f"{latitude},{longitude}")  # Use zone's location
+        temp, humidity, _ = get_weather_data(
+            f"{latitude},{longitude}")  # Use zone's location
 
         col_temp, col_humidity = st.columns(2, gap="small")
         col_temp.metric("Temperature (°C)", temp)
         col_humidity.metric("Humidité (%)", humidity)
-        col_event, col_add_event , col_disease = st.columns(3)
+        col_event, col_add_event, col_disease = st.columns(3)
         with col_event:
             st.write("### Événements")
         with col_add_event:
@@ -163,26 +172,27 @@ with col2:
                 analyze_image()
         calendar_events = []
         for event in get_events_by_zone(zone_id):
-            event_date = datetime.datetime.strptime(event.date, "%Y-%m-%d %H:%M:%S") if isinstance(event.date, str) else event.date
+            event_date = datetime.datetime.strptime(
+                event.date, "%Y-%m-%d %H:%M:%S") if isinstance(event.date, str) else event.date
             calendar_events.append(
-                    {
-                            "title": f" { event.name } :  {event.description}",
-                            "start": event_date.strftime("%Y-%m-%dT%H:%M:%S"),
-                            "backgroundColor": event.type,
-                    }
+                {
+                    "title": f" {event.name} :  {event.description}",
+                    "start": event_date.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "backgroundColor": event.type,
+                }
             )
 
-
-        calendar = calendar(events=calendar_events, custom_css=custom_css, options={"initialView": 'listWeek', "locale": 'fr'})
+        calendar = calendar(events=calendar_events, custom_css=custom_css, options={
+                            "initialView": 'listWeek', "locale": 'fr'})
 
         if st.button("Generer une nouvelle planification"):
             st.session_state.watering_plan = generate_detailed_watering_plan(
-                    plant_name=crop_type,
-                    water_frequency=4,
-                    base_amount=500,
-                    latitude=latitude,
-                    longitude=longitude,
-                    api_key=API_KEY
+                plant_name=crop_type,
+                water_frequency=4,
+                base_amount=500,
+                latitude=latitude,
+                longitude=longitude,
+                api_key=API_KEY
             )
             st.rerun()
 
@@ -192,27 +202,27 @@ with col2:
                 delete_all_zone_events_by_type(zone_id, "green")
                 date = datetime.datetime.strptime(plan["date"], "%Y-%m-%d")
                 for watering_event in plan["watering_schedule"]:
-                    st.write(f"Arrosage {date.strftime('%Y-%m-%d')} {watering_event['time']}")
+                    st.write(
+                        f"Arrosage {date.strftime('%Y-%m-%d')} {watering_event['time']}")
                     st.write(watering_event["description"])
 
             if st.button("Ajouter à l'agenda"):
                 for daily_plan in st.session_state.watering_plan["daily_plans"]:
 
-                    date = datetime.datetime.strptime(daily_plan["date"], "%Y-%m-%d")
+                    date = datetime.datetime.strptime(
+                        daily_plan["date"], "%Y-%m-%d")
                     for watering_event in daily_plan["watering_schedule"]:
                         add_event(
-                                name="Arrosage",
-                                date=datetime.datetime.combine(date, datetime.datetime.strptime(watering_event["time"], "%H:%M").time()),
-                                event_type="green",
-                                management_zone_id=zone_id,
-                                description=watering_event["description"]
+                            name="Arrosage",
+                            date=datetime.datetime.combine(date, datetime.datetime.strptime(
+                                watering_event["time"], "%H:%M").time()),
+                            event_type="green",
+                            management_zone_id=zone_id,
+                            description=watering_event["description"]
                         )
-                st.success("Tous les événements ont été ajoutés à l'agenda avec succès!")
+                st.success(
+                    "Tous les événements ont été ajoutés à l'agenda avec succès!")
                 st.session_state.watering_plan = None  # Reset the plan after adding to agenda
                 st.rerun()
     else:
         st.write("Veuillez sélectionner une zone de gestion")
-
-
-
-
